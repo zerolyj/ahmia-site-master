3fd6642064b3af46e8b16c76897fbba0fbea808a
zerolyj <441205099@qq.com>
2022-04-08 08:46:09 +0800

    first commit

---
+++ ahmia/ahmia/templatetags/ahmia_custom.py
@@ -0,0 +1,20 @@
+"""Tags used in ahmia's templates"""
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.assignment_tag
+def is_development_environment():
+    """Test if template is rendered in debug mode"""
+    return settings.DEBUG
+
+
+@register.simple_tag
+def get_environment():
+    """Return environment mode"""
+    if settings.DEBUG:
+        return 'development'
+    else:
+        return 'production'
